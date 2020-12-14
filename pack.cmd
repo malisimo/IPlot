79b15e6f720773ef5987e28251b7b8b438c4c748
Matt <mattrumba@gmail.com>
2020-12-14 22:13:23 +0000

    Packing three nuget packages

--- pack.cmd
+++ pack.cmd
@@ -1,3 +1,7 @@
 dotnet build .\src\IPlot\IPlot.fsproj -c Release
+dotnet build .\src\IPlot.Plotly\IPlot.Plotly.csproj -c Release
+dotnet build .\src\IPlot.HighCharts\IPlot.HighCharts.csproj -c Release
 
-nuget pack IPlot.nuspec
+dotnet pack .\src\IPlot\IPlot.fsproj -c Release -o .
+dotnet pack .\src\IPlot.Plotly\IPlot.Plotly.csproj -c Release -o .
+dotnet pack .\src\IPlot.HighCharts\IPlot.HighCharts.csproj -c Release -o .
