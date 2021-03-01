1a0e88b669d8c271383109d913739813ca22d94a
Matt <mattrumba@gmail.com>
2021-03-01 22:39:04 +0000

    Adding interactive project to batch files

--- buildsrc.cmd
+++ buildsrc.cmd
@@ -1,3 +1,4 @@
 dotnet build .\src\IPlot.Plotly\IPlot.Plotly.csproj
 dotnet build .\src\IPlot.HighCharts\IPlot.HighCharts.csproj
 dotnet build .\src\IPlot\IPlot.fsproj
+dotnet build .\src\IPlot.Interactive\IPlot.Interactive.fsproj
--- pack.cmd
+++ pack.cmd
@@ -1,7 +1,9 @@
-dotnet build .\src\IPlot\IPlot.fsproj -c Release
 dotnet build .\src\IPlot.Plotly\IPlot.Plotly.csproj -c Release
 dotnet build .\src\IPlot.HighCharts\IPlot.HighCharts.csproj -c Release
+dotnet build .\src\IPlot\IPlot.fsproj -c Release
+dotnet build .\src\IPlot.Interactive\IPlot.Interactive.fsproj -c Release
 
-dotnet pack .\src\IPlot\IPlot.fsproj -c Release -o .
-dotnet pack .\src\IPlot.Plotly\IPlot.Plotly.csproj -c Release -o .
-dotnet pack .\src\IPlot.HighCharts\IPlot.HighCharts.csproj -c Release -o .
+dotnet pack .\src\IPlot.Plotly\IPlot.Plotly.csproj -c Release -o pkg
+dotnet pack .\src\IPlot.HighCharts\IPlot.HighCharts.csproj -c Release -o pkg
+dotnet pack .\src\IPlot\IPlot.fsproj -c Release -o pkg
+dotnet pack .\src\IPlot.Interactive\IPlot.Interactive.fsproj -c Release -o pkg
