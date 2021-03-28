46c3f304f8f8384247162bb4760f6e85d4432416
Matt <mattrumba@gmail.com>
2021-03-28 22:51:19 +0100

    Remove old nuget packages on pack cmd

--- pack.cmd
+++ pack.cmd
@@ -3,6 +3,8 @@ dotnet build .\src\IPlot.HighCharts\IPlot.HighCharts.csproj -c Release
 dotnet build .\src\IPlot\IPlot.fsproj -c Release
 dotnet build .\src\IPlot.Interactive\IPlot.Interactive.fsproj -c Release
 
+del .\pkg\*.nupkg
+
 dotnet pack .\src\IPlot.Plotly\IPlot.Plotly.csproj -c Release -o pkg
 dotnet pack .\src\IPlot.HighCharts\IPlot.HighCharts.csproj -c Release -o pkg
 dotnet pack .\src\IPlot\IPlot.fsproj -c Release -o pkg
